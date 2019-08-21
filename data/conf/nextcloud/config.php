475e7954a1ef49169a6371deed200fde0ba73d0d
chinthakadeshapriya <chinthaka@cybergate.lk>
2019-08-21 12:58:16 +0530

    Initial add

---
+++ data/conf/nextcloud/config.php
@@ -0,0 +1,25 @@
+# Set your nextcloud container's config located at /config/www/nextcloud/config/config.php
+# and add the following lines before the ");":
+#  'trusted_proxies' => ['nginx'],
+#  'overwritewebroot' => '/nextcloud',
+#  'overwrite.cli.url' => 'https://${OPENEMAIL_HOSTNAME}/nextcloud',
+#
+# Also don't forget to add your domain name to the trusted domains array. It should look somewhat like this:
+#  array (
+#    0 => '192.168.0.1:444', # This line may look different on your setup, don't modify it.
+#    1 => '${YOURDOMAIN}',
+#  ),
+<?php
+$openemail_hostname = getenv('OPENEMAIL_HOSTNAME');
+$CONFIG = array (
+  'memcache.local' => '\OC\Memcache\APCu',
+  'datadirectory' => '/data',
+  'trusted_proxies' => ['nginx'],
+  'overwritewebroot' => '/nextcloud',
+  'overwrite.cli.url' => 'https://$openemail_hostname/nextcloud',
+  'trusted_domains' =>
+     array (
+     0 => 'nextcloud',
+     1 => '$openemail_hostname',
+   ),
+);
