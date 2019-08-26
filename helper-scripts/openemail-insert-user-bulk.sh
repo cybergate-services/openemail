2d022704b5168b9048450085177aa702a8016d37
chinthakadeshapriya <chinthaka@cybergate.lk>
2019-08-26 20:44:14 +0530

    Modified

--- helper-scripts/openemail-insert-user-bulk.sh
+++ helper-scripts/openemail-insert-user-bulk.sh
@@ -19,22 +19,23 @@ read -r -p "Are you sure you want to insert mailbox accounts? [y/N] " response
 response=${response,,}    # tolower
 if [[ "$response" =~ ^(yes|y)$ ]]; then
 	echo -e "\nWorking, please wait..."
-	for row in $(cat ./openemail-users.csv)
-	do
-		export ID=$(echo $row | cut -f1 -d ,) ; echo ${ID}
-		export USER=$(echo $row | cut -f2 -d ,) ; echo ${USER}
-		export FNAME=$(echo $row | cut -f3 -d ,) ; echo ${FNAME}
-		export DOMAIN=$(echo $row | cut -f4 -d ,) ; echo ${DOMAIN}
-		export EMAIL=$(echo $row | cut -f5 -d ,) ; echo ${EMAIL}
-		export QUOTA=$(echo $row | cut -f6 -d ,) ; echo ${QUOTA}
-		export PWHASH=$(echo $row | cut -f7 -d ,) ; echo ${PWHASH}
+	filename="./openemail-users.csv"
+  while read -r line; do
+    row="$line"
+    export ID=$(echo $row | cut -f1 -d ,) ; echo ${ID}
+    export USER=$(echo $row | cut -f2 -d ,) ; echo ${USER}
+    export FNAME=$(echo $row | cut -f3 -d ,) ; echo ${FNAME}
+    export DOMAIN=$(echo $row | cut -f4 -d ,) ; echo ${DOMAIN}
+    export EMAIL=$(echo $row | cut -f5 -d ,) ; echo ${EMAIL}
+    export QUOTA=$(echo $row | cut -f6 -d ,) ; echo ${QUOTA}
+    export PWHASH=$(echo $row | cut -f7 -d ,) ; echo ${PWHASH}
 		docker exec -it $(docker ps -qf name=mysql) mysql -u${DBUSER} -p${DBPASS} ${DBNAME} -e "SET @ID=${ID};SET @USER='${USER}';SET @FNAME='${FNAME}';SET @DOMAIN='${DOMAIN}';SET @EMAIL='${EMAIL}';SET @QUOTA=${QUOTA};SET @PWHASH='${PWHASH}';"
 		docker exec -it $(docker ps -qf name=mysql) mysql -u${DBUSER} -p${DBPASS} ${DBNAME} -e "INSERT INTO mailbox (username, password, name, quota, local_part, domain, attributes, active) VALUES (@EMAIL, @PWHASH, @FNAME, @QUOTA, @USER, @DOMAIN, '{\"force_pw_update\":\"0\",\"tls_enforce_in\":\"0\",\"tls_enforce_out\":\"0\",\"sogo_access\":\"1\",\"mailbox_format\":\"maildir:\",\"quarantine_notification\":\"never\"}', 1);"
     docker exec -it $(docker ps -qf name=mysql) mysql -u${DBUSER} -p${DBPASS} ${DBNAME} -e "INSERT INTO alias (id, address, goto, domain, active) VALUES (@ID, @EMAIL, @EMAIL, @DOMAIN, 1);"
 	  docker exec -it $(docker ps -qf name=mysql) mysql -u${DBUSER} -p${DBPASS} ${DBNAME} -e "INSERT INTO quota2 (username, bytes, messages) VALUES (@EMAIL, 0, 0);"
     docker exec -it $(docker ps -qf name=mysql) mysql -u${DBUSER} -p${DBPASS} ${DBNAME} -e "INSERT INTO user_acl (username, spam_alias, tls_policy, spam_score, spam_policy, delimiter_action, syncjobs, eas_reset, sogo_profile_reset, quarantine, quarantine_attachments, quarantine_notification) VALUES (@EMAIL, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);"
 	  docker exec -it $(docker ps -qf name=mysql) mysql -u${DBUSER} -p${DBPASS} ${DBNAME} -e "INSERT INTO _sogo_static_view (c_uid, domain, c_name, c_password, c_cn, mail, aliases, ad_aliases, kind, multiple_bookings) VALUES (@EMAIL, @DOMAIN, @EMAIL, @PWHASH, @FNAME, @EMAIL, '', '', '', -1);"
-	done
+	done < "$filename"	
 	echo "
 User successfully added
 "
