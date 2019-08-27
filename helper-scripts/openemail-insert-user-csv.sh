9105f781242f6b6834f38e2f8ff45f99c09921ce
chinthakadeshapriya <chinthaka@cybergate.lk>
2019-08-27 14:59:35 +0530

    Modified

--- helper-scripts/openemail-insert-user-csv.sh
+++ helper-scripts/openemail-insert-user-csv.sh
@@ -19,11 +19,14 @@ read -r -p "Are you sure you want to insert mailbox accounts? [y/N] " response
 response=${response,,}    # tolower
 if [[ "$response" =~ ^(yes|y)$ ]]; then
 	echo -e "\nWorking, please wait..."
-	docker exec -it $(docker ps -qf name=mysql) mysql -u${DBUSER} -p${DBPASS} ${DBNAME} -e "INSERT INTO mailbox (username, password, name, quota, local_part, domain, attributes, active) VALUES ('helpdesk@softlineglobal.com', '{SSHA256}4BrbE0bJqOcY//mYmqSbHCPjx+GyAYZ1NCnr50lFtl1lZDJmMDdiMGQ5NDkxMmRh', 'Softline helpdesk',  5343543296, 'helpdesk', 'softlineglobal.com', '{\"force_pw_update\":\"0\",\"tls_enforce_in\":\"0\",\"tls_enforce_out\":\"0\",\"sogo_access\":\"1\",\"mailbox_format\":\"maildir:\",\"quarantine_notification\":\"never\"}', 1);"
-  docker exec -it $(docker ps -qf name=mysql) mysql -u${DBUSER} -p${DBPASS} ${DBNAME} -e "INSERT INTO alias (id, address, goto, domain, active) VALUES (11, 'helpdesk@softlineglobal.com', 'helpdesk@softlineglobal.com', 'softlineglobal.com', 1);"
-	docker exec -it $(docker ps -qf name=mysql) mysql -u${DBUSER} -p${DBPASS} ${DBNAME} -e "INSERT INTO quota2 (username, bytes, messages) VALUES ('helpdesk@softlineglobal.com', 0, 0);"
-  docker exec -it $(docker ps -qf name=mysql) mysql -u${DBUSER} -p${DBPASS} ${DBNAME} -e "INSERT INTO user_acl (username, spam_alias, tls_policy, spam_score, spam_policy, delimiter_action, syncjobs, eas_reset, sogo_profile_reset, quarantine, quarantine_attachments, quarantine_notification) VALUES ('helpdesk@softlineglobal.com', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);"
-	docker exec -it $(docker ps -qf name=mysql) mysql -u${DBUSER} -p${DBPASS} ${DBNAME} -e "INSERT INTO _sogo_static_view (c_uid, domain, c_name, c_password, c_cn, mail, aliases, ad_aliases, kind, multiple_bookings) VALUES ('helpdesk@softlineglobal.com', 'softlineglobal.com', 'helpdesk@softlineglobal.com', '{SSHA256}4BrbE0bJqOcY//mYmqSbHCPjx+GyAYZ1NCnr50lFtl1lZDJmMDdiMGQ5NDkxMmRh', 'Softline helpdesk', 'helpdesk@softlineglobal.com', '', '', '', -1);"
+
+	echo "done"
+	IFS=,
+	while read id user fname domain email quota pwhash
+  do
+    echo "INSERT INTO mailbox (username, password, name, quota, local_part, domain, attributes, active) VALUES ('$email', '$fwhash', '$fname',  $quota, '$user', '$domain', '{\"force_pw_update\":\"0\",\"tls_enforce_in\":\"0\",\"tls_enforce_out\":\"0\",\"sogo_access\":\"1\",\"mailbox_format\":\"maildir:\",\"quarantine_notification\":\"never\"}', 1);"
+	done < openemail-users.csv | docker exec -it $(docker ps -qf name=mysql) mysql -u${DBUSER} -p${DBPASS} ${DBNAME} -e;
+	echo "finish"
 	echo "
 User successfully added
 "
