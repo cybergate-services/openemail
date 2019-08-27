dc1a403ff4e345eff4d85e866504b0d6b3592f46
chinthakadeshapriya <chinthaka@cybergate.lk>
2019-08-27 17:08:32 +0530

    Modified

--- helper-scripts/openemail-insert-user-csv.sh
+++ helper-scripts/openemail-insert-user-csv.sh
@@ -26,9 +26,42 @@ if [[ "$response" =~ ^(yes|y)$ ]]; then
   do
     echo "INSERT INTO mailbox (username, password, name, quota, local_part, domain, attributes, active) VALUES ('$email', '$pwhash', '$fname',  $quota, '$user', '$domain', '{\"force_pw_update\":\"0\",\"tls_enforce_in\":\"0\",\"tls_enforce_out\":\"0\",\"sogo_access\":\"1\",\"mailbox_format\":\"maildir:\",\"quarantine_notification\":\"never\"}', 1);"
 	done < openemail-users.csv | docker-compose exec -T mysql-openemail mysql -u${DBUSER} -p${DBPASS} ${DBNAME} ;
-	echo "finish"
+	echo "Added to mailbox table"
+
+	echo "done"
+	IFS=,
+	while read id user fname domain email quota pwhash
+  do
+    echo "INSERT INTO alias (id, address, goto, domain, active) VALUES ($id, '$email', '$email', '$domain', 1);"
+	done < openemail-users.csv | docker-compose exec -T mysql-openemail mysql -u${DBUSER} -p${DBPASS} ${DBNAME} ;
+	echo "Added to alias table"
+
+	echo "done"
+	IFS=,
+	while read id user fname domain email quota pwhash
+  do
+    echo "INSERT INTO quota2 (username, bytes, messages) VALUES ('$email', 0, 0);"
+	done < openemail-users.csv | docker-compose exec -T mysql-openemail mysql -u${DBUSER} -p${DBPASS} ${DBNAME} ;
+	echo "Added to quota2 table"
+
+	echo "done"
+	IFS=,
+	while read id user fname domain email quota pwhash
+  do
+    echo "INSERT INTO user_acl (username, spam_alias, tls_policy, spam_score, spam_policy, delimiter_action, syncjobs, eas_reset, sogo_profile_reset, quarantine, quarantine_attachments, quarantine_notification) VALUES ('$email', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);"
+	done < openemail-users.csv | docker-compose exec -T mysql-openemail mysql -u${DBUSER} -p${DBPASS} ${DBNAME} ;
+	echo "Added to user_acl table"
+
+	echo "done"
+	IFS=,
+	while read id user fname domain email quota pwhash
+  do
+    echo "INSERT INTO _sogo_static_view (c_uid, domain, c_name, c_password, c_cn, mail, aliases, ad_aliases, kind, multiple_bookings) VALUES ('$email', '$domain', '$email', '$pwhash', '$fname', '$email', '', '', '', -1);"
+	done < openemail-users.csv | docker-compose exec -T mysql-openemail mysql -u${DBUSER} -p${DBPASS} ${DBNAME} ;
+	echo "Added to _sogo_static_view table"
+
 	echo "
-User successfully added
+mailbox accounts successfully added to all required tables.
 "
 else
 	echo "Operation canceled."
